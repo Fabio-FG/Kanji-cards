d020e481c5cf3fdcd824812af6511869ff06d121
fabioFG <fabio.fernando.guerreiro@gmail.com>
2022-03-06 12:07:14 +0000

    Japan-flashcard:
     Added:
      - Router dom;
      - Imported dependencies;
      - Homepage view;
      - Component folders;

---
+++ src/pages/homepage/HomePage.tsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const HomePage:React.FC = () => {
+    return ( 
+        <div>This is a Kanji app</div>
+
+    );
+}
+
+export default HomePage ;
