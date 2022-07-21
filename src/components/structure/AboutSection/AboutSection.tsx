e886e4d88c3840d8993bc23017bb4467932cb3d9
fabioFG <fabio.fernando.guerreiro@gmail.com>
2022-07-21 13:25:39 +0100

    added svgs and styling

--- src/components/structure/AboutSection/AboutSection.tsx
+++ src/components/structure/AboutSection/AboutSection.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import "./styles.css";
 
+
 export default function AboutSection() {
   const navigate = useNavigate();
 
@@ -10,24 +11,24 @@ export default function AboutSection() {
 
   return (
     <section className="about">
-      <div className="about-intro">
-        <h1 className="about-title">About LearnKanji 漢字</h1>
-        <text className="about-text">
-          <p>
-            LearnKanji is a personal project geared towards those who wish to
-            learn and study Japanese kanji on a daily basis. The project is
-            merely an enhancement tool and the goal is to be an auxiliary method
-            in learning.
-          </p>
-          <br />
-          <p>
-            This project is divided into{" "}
-            <span className="span" onClick={goToAbout}>
-              three phases.
-            </span>
-          </p>
-        </text>
-      </div>
+      <article className="about-text">
+      <h1 className="about-title">About LearnKanji 漢字</h1>
+        <p>
+          LearnKanji is a personal project geared towards those who wish to
+          learn and study Japanese kanji on a daily basis. The project is merely
+          an enhancement tool and the goal is to be an auxiliary method in
+          learning.
+        </p>
+        <br />
+        <p>
+          This project is divided into{" "}
+          <span className="span" onClick={goToAbout}>
+            three phases.
+          </span>
+        </p>
+      </article>
+     
+
     </section>
   );
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import './HomePage.css';
+import "./HomePage.css";
 import useFetch from "../../Hooks/useFetch";
 import KanjiList from "../../components/KanjiList/KanjiList";
 import HeaderSection from "../../components/structure/HeaderSection/HeaderSection";
-import bg from "../../assets/images/WallpaperDog-10848836.jpg"
+import bg from "../../assets/images/WallpaperDog-10848836.jpg";
 import AboutSection from "../../components/structure/AboutSection/AboutSection";
+
 
 const HomePage = () => {
   //custom hook to fetch data
@@ -12,19 +13,22 @@ const HomePage = () => {
     "https://kanjiapi.dev/v1/kanji/all"
   );
 
-
   return (
     <>
       {error && <div>{error}</div>}
       {!isPending && <div>Loading...</div>}
       <div className="container">
         <div>
-          <img src={bg} alt="intro-background" className="homepageBg"/>
+          <img src={bg} alt="intro-background" className="homepageBg" />
           <HeaderSection />
-          <AboutSection />
+          
+            <AboutSection />
+         
         </div>
         <h3 className="kanji-subtitle">An example of how the app works</h3>
-        <div className="data">{data && <KanjiList {...data as Object} />}</div>
+        <div className="data">
+          {data && <KanjiList {...(data as Object)} />}
+        </div>
       </div>
     </>
   );
